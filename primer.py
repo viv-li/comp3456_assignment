3dbf86bbec826451d9bea4a7081c4222ef364432
Vivian Li <l.vivian.93@gmail.com>
2013-10-20 13:45:26 +1100

    *Added more primer parameter checks, better output format

--- primer.py
+++ primer.py
@@ -1,15 +1,17 @@
 import math
 
 class Primer:
-    def __init__(self, target, start=0, end=18):
-        self.start = start
-        self.end = end
-        self.seq = target.seq[start:end]
-        self.len = end-start
+    def __init__(self, target, start, end):
+        self.start = start                  #index of primer start in target seq
+        self.end = end                      #index of primer end in target seq
+        self.seq = target.seq[start:end]    #primer dna sequence
+        self.target = target                #target dna as Target object
+        self.len = end-start                #length of primer
     
     def __repr__(self):
         return "5'-"+self.seq+"-3'"
 
+    ## Returns the reverse complement of the primer for self annealing purposes
     def reverse_complement(self):
         complement = {"a":"t", "c":"g", "g":"c", "t":"a"}
         seq = self.seq[::-1]
@@ -20,13 +22,13 @@ class Primer:
             
     ## Calculate melting temperature of the primer, given salt concentration set to default 50 nM
     def mt(self, salt=0.05):
-        Rlnc4 = -36.1585
-        tot = -273.15+(16.6*math.log(salt, 10))
+        #Enthalpy and entropy values for pairs of nucleotides from nearest neighbour thermodynamics model
         nn = {"aa":(9.1,24),"tt":(9.1,24),"at":(8.6,23.9),"ta":(6.0,16.9),\
               "ca":(5.8,12.9),"tg":(5.8,12.9),"gt":(6.5, 17.3),"ac":(6.5, 17.3),\
               "ct":(7.8, 20.8),"ag":(7.8, 20.8),"ga":(5.6, 13.5),"tc":(5.6, 13.5),\
               "cg":(11.9, 27.8),"gc":(11.1, 26.7),"gg":(11.0, 26.6),"cc":(11.0, 26.6)}
-
+        Rlnc4 = -36.1585
+        tot = -273.15+(16.6*math.log(salt, 10))
         dH = 0
         dS = 0
         for i in range(self.len-1):
@@ -45,9 +47,48 @@ class Primer:
 
     ## Returns boolean as to whether GC clamp at 3' end of primer exists
     def gc_clamp(self):
-        if self.seq[-1] in "gc" and self.seq[-2] in "gc":
-            return True
+        for pattern in ["cc","cg","gc","gg"]:
+            if pattern in self.seq[-5:]:
+                return True
         return False
+
+    ## Returns boolean as to whether primer contains a homooligomer of length 5 or greater
+    def homooligomer(self):
+        for i in range(self.len-5):
+            if self.seq[i:i+5] in ["aaaaa","ccccc","ggggg","ttttt"]:
+                return True
+        return False
+
+    ## Returns boolean as to whether primer contains > 3 x dinucleotide repeat
+    def dinucleotide_repeat(self):
+        for i in range(self.len-6):
+            if self.seq[i:i+2] == self.seq[i+2:i+4] and\
+               self.seq[i+2:i+4] == self.seq[i+4:i+6]:
+                return True
+        return False
+
+    ## Returns boolean as to whether primer is specific based on following criteria:
+    ## no more than 3 G/C in last 5 bases or primer and the last 7 bases of primer
+    ## with up to 1 mutation doesn't occur anywhere else in target sequence
+    def specificity(self):
+        gc_count = 0
+        gc_count += self.seq[-5:].count("g")
+        gc_count += self.seq[-5:].count("c")
+        if gc_count > 3:
+            return False
+
+        to_match = []
+        seq3 = self.seq[-7:]
+        for i in range(7):
+            for n in ['a','c','g','t']:
+                s = seq3[:i] + n + seq3[i+1:]
+                to_match.append(s)
+
+        for s in to_match:
+            if self.target.seq.count(s) > 1:
+                return False
+        return True
+            
 
     ## Takes a primer and determines self-alignments with largest
     ## self-annealing and self-end-annealing scores.
@@ -76,11 +117,11 @@ class Primer:
             if i > self.len:
                 alignment = "5'-"+self.seq+"-3'"+"\n"\
                             + " "*(i-self.len+4)+matches + "\n"\
-                            +" "*(i-self.len+1)+"3'-"+self.seq[::-1]+"-5'"
+                            +" "*(i-self.len+1)+"3'-"+self.seq[::-1]+"-5'\n"
             else:
                 alignment = " "*(self.len-i-1)+"5'-"+self.seq+"-3'"+"\n"\
                             +"   "+matches + "\n"\
-                            +"3'-"+self.seq[::-1]+"-5'"
+                            +"3'-"+self.seq[::-1]+"-5'\n"
                 
             if score > max_sa:
                 max_sa = score
@@ -94,13 +135,9 @@ class Primer:
                     sea_alignments = [alignment]
                 elif score == max_sea:
                     sea_alignments.append(alignment)
-
-##        for a in sa_alignments:
-##            print(a)
-##        for a in sea_alignments:
-##            print(a)
             
-        return (max_sa, max_sea)
+        return (max_sa, "\n".join(sa_alignments), max_sea, "\n".join(sea_alignments))
+
 
     ## Takes a forward and reverse primer pair and determines alignments with largest
     ## pair-annealing and pair-end-annealing scores.
@@ -129,11 +166,11 @@ class Primer:
             if i > self.len:
                 alignment = "5'-"+self.seq+"-3'"+"\n"\
                             + " "*(i-self.len+4)+matches + "\n"\
-                            +" "*(i-self.len+1)+"3'-"+other.seq[::-1]+"-5'"
+                            +" "*(i-self.len+1)+"3'-"+other.seq[::-1]+"-5'\n"
             else:
                 alignment = " "*(other.len-i-1)+"5'-"+self.seq+"-3'"+"\n"\
                             +"   "+matches + "\n"\
-                            +"3'-"+other.seq[::-1]+"-5'"
+                            +"3'-"+other.seq[::-1]+"-5'\n"
             
             if score > max_pa:
                 max_pa = score
@@ -147,37 +184,60 @@ class Primer:
                     pea_alignments = [alignment]
                 elif score == max_pea:
                     pea_alignments.append(alignment)
+        return (max_pa, "\n".join(pa_alignments), max_pea, "\n".join(pea_alignments))
+    
 
-##        for a in pa_alignments:
-##            print(a)
-##        for a in pa_alignments:
-##            print(a)
-            
-        return (max_pa, max_pea)        
+    ## Returns the following primer parameters: mt, gc content, gc_clamp
+    ## homooligomerisation, dinucleotide repeat, specificity
+    def parameters(self):
+        return (self.mt(), self.gc_content(), self.gc_clamp(), self.homooligomer(),\
+                self.dinucleotide_repeat(), self.specificity())
 
-    ## Takes a forward and reverse primer pair and
-    ## returns their scoring vector
+    ## Takes a forward and reverse primer pair and returns scoring vector:
+    ## length, gc_content, mt, sa and sea of both, and pa and pea scores
     def scoring_vector(self, other):
-        sa_p, sea_p = self.self_annealing()
-        sa_q, sea_q = other.self_annealing()
-        pa, pea = self.pair_annealing(other)
+        sa_p, sa_pa, sea_p, sea_pa = self.self_annealing()
+        sa_q, sa_qa, sea_q, sea_qa = other.self_annealing()
+        pa, pa_a, pea, pea_a = self.pair_annealing(other)
         return (self.len, other.len, self.gc_content(), other.gc_content(),\
                 self.mt(), other.mt(), sa_p, sa_q, sea_p, sea_q, pa, pea)
-        
-    def self_score(self):
-        return (self.mt(), self.gc_content(), self.gc_clamp())
-        
+
+
+
+class Probe:
+    def __init__(self, target):
+        pass
+
 
 class Target:
-    def __init__(self, seq, start, end):
-        self.seq = seq
-        self.start = start
-        self.end = end
-        self.len = len(seq)
+    def __init__(self, seq, start, end, is_forward):
+        self.seq = seq                  #sequence of entire dna fragment
+        self.start = start              #index of start of desired target seq
+        self.end = end                  #index of end of desired target seq
+        self.len = end-start            #legnth of target sequence
+        self.is_forward = is_forward    #boolean as to whether strand is the forward strand
 
     def __repr__(self):
-        return "5'-" + self.seq[:self.start]+self.seq[self.start:self.end].upper()\
-               +self.seq[self.end:] + "-3'"
+        fseq = self.seq
+        fseq = fseq[:self.start]+fseq[self.start:self.end].upper()+fseq[self.end:]
+        
+        rstrand = self.reverse_complement()
+        rseq = rstrand.seq
+        rseq = rseq[:rstrand.start]+rseq[rstrand.start:rstrand.end].upper()+rseq[rstrand.end:]
+        rseq = rseq[::-1]
+        
+        chunks = []
+        count = 0
+        while len(fseq) > 100:
+            chunks.append(str(count*100+1)+" 5'-"+fseq[:100]+"-3' "+str(count*100+100)+"\n"+
+                          " "*len(str(count*100+1))+" 3'-"+rseq[:100]+"-5'")
+            fseq = fseq[100:]
+            rseq = rseq[100:]
+            count += 1
+        if len(fseq) != 0:
+            chunks.append(str(count*100+1)+" 5'-"+fseq+"-3' "+str(len(self.seq))+"\n"+
+                          " "*len(str(count*100+1))+" 3'-"+rseq+"-5'")
+        return "\n\n".join(chunks)+"\n"
         
     def reverse_complement(self):
         complement = {"a":"t", "c":"g", "g":"c", "t":"a"}
@@ -185,11 +245,18 @@ class Target:
         rseq = ""
         for letter in seq:
             rseq += complement[letter]
-        start = self.len - self.end
-        end = self.len - self.start
-        rc = Target(rseq, start, end)
+        start = len(self.seq) - self.end
+        end = len(self.seq) - self.start
+        is_forward = not self.is_forward
+        rc = Target(rseq, start, end, is_forward)
         return rc
 
+    def gc_content(self):
+        count = 0
+        for letter in self.seq[self.start:self.end]:
+            if letter in "gc":
+                count += 1
+        return count*100/(self.end-self.start)
                
 
 
--- primer_search.py
+++ primer_search.py
@@ -1,70 +1,98 @@
 from primer import Primer
 from primer import Target
 
-def weighted_distance(sc, sc_ideal):
+def clean_dna(seq):
+    cleaned = ""
+    for letter in seq.lower():
+        if letter in "acgt":
+            cleaned += letter
+    return cleaned
+
+#given the scoring vector 
+def weighted_distance(sv):
     wd = 0
     ##distances with weights applied
-    wd += abs(sc[0] - sc[1])*0.5 ##equal length
-    wd += abs(sc[2] - sc[3]) ##equal gc content
-    wd += abs(sc[4] - sc[5]) ##equal melting temp
-    wd += abs(sc[6] - sc_ideal[6])*0.1 ##forward sa
-    wd += abs(sc[7] - sc_ideal[7])*0.1 ##reverse sa
-    wd += abs(sc[8] - sc_ideal[8])*0.2 ##forward sea
-    wd += abs(sc[9] - sc_ideal[9])*0.2 ##reverse sea
-    wd += abs(sc[10] - sc_ideal[10])*0.1 ##pa
-    wd += abs(sc[11] - sc_ideal[11])*0.2 ##pea
-
+    wd += abs(sv[0] - sv[1])*0.5 ##equal length
+    wd += abs(sv[2] - sv[3]) ##equal gc content
+    wd += abs(sv[4] - sv[5]) ##equal melting temp
+    wd += sv[6]*0.1 ##forward sa
+    wd += sv[7]*0.1 ##reverse sa
+    wd += sv[8]*0.2 ##forward sea
+    wd += sv[9]*0.2 ##reverse sea
+    wd += sv[10]*0.1 ##pa
+    wd += sv[11]*0.2 ##pea
     return wd
 
 
+## given a single strand of DNA, find all acceptable primers
+## mt, gc content, gc clamp or no, no homooligomers, no dinucleotide repeats, specificity
 def find_primers(target_dna, user_parameters):
     primers = []
-    for i in range(target_dna.start+1):
-        for j in range(12, 21):
+    if target_dna.len < 500:
+        ideal_len = (16, 18)
+        search_range = 50
+    elif target_dna.len > 2500:
+        ideal_len = (20, 24)
+        search_range = 250
+    else:
+        ideal_len = (18, 20)
+        search_range = 100
+    search_start = max(0, target_dna.start-search_range)
+    
+    for i in range(search_start, target_dna.start+1):
+        for j in range(ideal_len[0], ideal_len[1]+1):
             primer = Primer(target_dna, i, i+j)
-            parameters = primer.self_score()
+            parameters = primer.parameters()
             
             if user_parameters[0][0] <= parameters[0] <= user_parameters[0][1] and\
-               user_parameters[1][0] <= parameters[1] <= user_parameters[1][1]:
-                if user_parameters[2] == 1:
-                    if parameters[2] ==1:
-                        primers.append(primer)
-                else:
+               user_parameters[1][0] <= parameters[1] <= user_parameters[1][1] and\
+               not parameters[3] and not parameters[4] and parameters[5]:
+                if user_parameters[2] == None:
                     primers.append(primer)
-                    
+                else:
+                    if user_parameters[2] == parameters[2]:
+                        primers.append(primer)                   
     return primers
 
-def score_pairs(fprimers, rprimers, ideal_sv):
+## given lists of forward and reverse primers, pair each up and calculate
+## scoring vector, return 5 best pairs
+def score_pairs(fprimers, rprimers):
     scores = []
     i = 0
     for f in fprimers:
         j = 0
         for r in rprimers:
-            scores.append((weighted_distance(f.scoring_vector(r), ideal_sv), i, j))
+            scores.append((weighted_distance(f.scoring_vector(r)), i, j))
             j += 1
         i += 1
     scores.sort()
     return scores[:5]
 
 
-fstrand = Target("gattaagtttctagtcaccgctcgatatgctagctagactggctacg", 10, 30)
+sequence = clean_dna("".join(open("hiv.fasta","rU").read().split("\n")[1:]))
+fstrand = Target(sequence, 5771, 8341, True)
 rstrand = fstrand.reverse_complement()
-print(fstrand)
-print(rstrand)
-
-user_parameters = ((50,60), (40,60), 0)
-fprimers = find_primers(fstrand, user_parameters)
-rprimers = find_primers(rstrand, user_parameters)
-##print(fprimers)
-##print(rprimers)
-
-ideal_sv = (18, 18, 50, 50, 55, 55, 0,0,0,0,0,0)
-
-for score in score_pairs(fprimers, rprimers, ideal_sv):
-    print(score[0], fprimers[score[1]], rprimers[score[2]], fprimers[score[1]].scoring_vector(rprimers[score[2]]))
 
 ##mt, gc content, gc clamp defaults
-min_len = 12
-max_len = 20
+user_parameters = ((57,63), (fstrand.gc_content()-5,fstrand.gc_content()+10), None)
+fprimers = find_primers(fstrand, user_parameters)
+rprimers = find_primers(rstrand, user_parameters)
+
+count = 1
+for result in score_pairs(fprimers, rprimers):
+    fprimer = fprimers[result[1]]
+    fself = fprimer.self_annealing()
+    rprimer = rprimers[result[2]]
+    rself = rprimer.self_annealing()
+    pair = fprimer.pair_annealing(rprimer)
+    sv = fprimer.scoring_vector(rprimer)
+    Ta = min(sv[4],sv[5])-5
+
+    print("{:d}. Forward primer: {:s} start = {:d}, length = {:d}bp, %GC = {:f}, Tm = {:.2f}°C".format(count,fprimer,fprimer.start,sv[0],sv[2],sv[4]))
+    print("   Reverse primer: {:s} start = {:d}, length = {:d}bp, %GC = {:f}, Tm = {:.2f}°C".format(rprimer,len(fstrand.seq)-rprimer.start,sv[1],sv[3],sv[5]))
+    print("   Product size = {:d}bp, Ta = {:.2f}°C".format(len(fstrand.seq)-rprimer.start-fprimer.start, Ta))
+##    print("\n".join(map(str,pair)))
+    print()
+    count += 1
 
  
