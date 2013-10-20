61492c4164f25103e1fbc4c952d7ded44732839c
Vivian Li <l.vivian.93@gmail.com>
2013-10-20 14:09:33 +1100

    *Added cgi file

--- primer_search.py
+++ primer_search.py
@@ -67,32 +67,4 @@ def score_pairs(fprimers, rprimers):
         i += 1
     scores.sort()
     return scores[:5]
-
-
-sequence = clean_dna("".join(open("hiv.fasta","rU").read().split("\n")[1:]))
-fstrand = Target(sequence, 5771, 8341, True)
-rstrand = fstrand.reverse_complement()
-
-##mt, gc content, gc clamp defaults
-user_parameters = ((57,63), (fstrand.gc_content()-5,fstrand.gc_content()+10), None)
-fprimers = find_primers(fstrand, user_parameters)
-rprimers = find_primers(rstrand, user_parameters)
-
-count = 1
-for result in score_pairs(fprimers, rprimers):
-    fprimer = fprimers[result[1]]
-    fself = fprimer.self_annealing()
-    rprimer = rprimers[result[2]]
-    rself = rprimer.self_annealing()
-    pair = fprimer.pair_annealing(rprimer)
-    sv = fprimer.scoring_vector(rprimer)
-    Ta = min(sv[4],sv[5])-5
-
-    print("{:d}. Forward primer: {:s} start = {:d}, length = {:d}bp, %GC = {:f}, Tm = {:.2f}°C".format(count,fprimer,fprimer.start,sv[0],sv[2],sv[4]))
-    print("   Reverse primer: {:s} start = {:d}, length = {:d}bp, %GC = {:f}, Tm = {:.2f}°C".format(rprimer,len(fstrand.seq)-rprimer.start,sv[1],sv[3],sv[5]))
-    print("   Product size = {:d}bp, Ta = {:.2f}°C".format(len(fstrand.seq)-rprimer.start-fprimer.start, Ta))
-##    print("\n".join(map(str,pair)))
-    print()
-    count += 1
-
  
